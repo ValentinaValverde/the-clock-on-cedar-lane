ff506c202be0529b56d6391a983f689502e6fb58
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-21 22:19:53 -0500

    tweaks to cta section

--- app/components/CTASection.tsx
+++ app/components/CTASection.tsx
@@ -1,9 +1,11 @@
 export default function CTASection() {
   return (
     <div className="bg-light-green px-6 py-16 md:py-24">
-      <div className="max-w-3xl mx-auto text-center">
-        <p className="text-sm md:text-base text-black mb-4">Craving already?</p>
-        <h2 className="text-4xl md:text-5xl lg:text-6xl text-black leading-tight genty">
+      <div className="max-w-3xl mx-auto text-center ">
+        <p className="text-sm md:text-base text-black mb-8">
+          Drooling already?
+        </p>
+        <h2 className="text-4xl md:text-5xl lg:text-6xl text-black genty">
           Order online – we&apos;ll
           <br className="hidden md:block" />
           take care of the rest.
