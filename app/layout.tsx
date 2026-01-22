cbcb24da5d5a7f3be04329c6715d6a06f5a23412
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-22 00:21:50 -0500

    add smooth scroll behavior to anchor link (navbar menu button)

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`${gentyFont.variable} ${poppins.variable} antialiased`}>
         <Navbar />
         <div>{children}</div>
