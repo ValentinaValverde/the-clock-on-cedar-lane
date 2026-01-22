9382827339280108b56bb095b34bf10dfe7feadf
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-22 00:08:19 -0500

    fill in the rest of the menu cards

--- app/components/MenuSection.tsx
+++ app/components/MenuSection.tsx
@@ -17,14 +17,18 @@ export default function MenuSection() {
   return (
     <div className="w-full bg-dark-green flex flex-col items-center justify-center">
       <div className="w-2/3 py-24 flex flex-col items-center" id="menu">
-        <h2 className="text-5xl lg:text-6xl text-light-green genty mb-8">
+        <h2 className="text-5xl lg:text-6xl text-light-green genty ">
           Our Menu
         </h2>
+
+        <p className="text-center text-light-green mb-8">
+          First responders on duty come in and get 25% off!
+        </p>
 
         {/* menu #1 container */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-light-green">
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Sides</p>
+            <p className="text-center genty text-3xl mb-4">Sides</p>
 
             {sidesData.map((data, index) => (
               <div key={index}>
@@ -34,21 +38,65 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Cedar Lane Specials</p>
-          </div>
-
-          <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Kids Menu</p>
-          </div>
-
-          <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Drinks</p>
-          </div>
-
-          <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">
-              First responders on duty come in and get 25% off!
+            <p className="text-center genty text-3xl mb-4">
+              Cedar Lane Specials
             </p>
+
+            <div className="text-center mb-4">
+              <p className="text-2xl font-semibold">Cheeseburger Plate & Tea</p>
+              <p>
+                Lettuce, tomato, mayonnaise on the burger, served with fries and
+                coleslaw
+              </p>
+              <p className="font-semibold">$8.99</p>
+            </div>
+
+            <div className="text-center mb-4">
+              <p className="text-2xl font-semibold">
+                Hot Dog or Corn Dog Plate & Tea
+              </p>
+              <p>Served with fries and coleslaw</p>
+              <p className="font-semibold">$7.99</p>
+            </div>
+
+            <div className="text-center mb-4">
+              <p className="text-2xl font-semibold">Philly Steak Plate & Tea</p>
+              <p>
+                Mayonaisse, grilled pepers, and onions served with fries and
+                coleslaw
+              </p>
+              <p className="font-semibold">$12.99</p>
+            </div>
+          </div>
+
+          <div className="border border-light-green p-8 rounded-xl overflow-hidden text-center">
+            <p className="genty text-3xl">Kids&apos; Menu</p>
+            <p className="genty text-3xl mb-4">$5.99</p>
+
+            <div className="mb-4">
+              <p>Grilled Cheese</p>
+              <p>Hot Dog</p>
+              <p>Corn Dog</p>
+              <p>Chicken Fingers</p>
+              <p>Hamburger</p>
+              <p>Cheeseburger (+$0.50)</p>
+            </div>
+
+            <p className="text-xs italic">
+              Served with fries and a 12 oz. drink
+            </p>
+          </div>
+
+          <div className="border border-light-green p-8 rounded-xl overflow-hidden">
+            <p className="text-center genty text-3xl mb-4">Drinks</p>
+
+            <p className="text-center mb-8">
+              Pepsi - Diet Pepsi - Starry - Dr. Pepper - Mountain Dew -
+              Tropicana Lemonade - Sweet / Unsweet Tea
+            </p>
+
+            <MenuRow name="Medium" price="2.35" />
+            <MenuRow name="Jumbo" price="3.35" />
           </div>
         </div>
 
@@ -60,7 +108,7 @@ export default function MenuSection() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-light-green">
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Burgers</p>
+            <p className="text-center genty text-3xl mb-4">Burgers</p>
 
             <div className="flex justify-end gap-4">
               <p>Sandwich</p>
@@ -79,7 +127,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Subs</p>
+            <p className="text-center genty text-3xl mb-4">Subs</p>
 
             <div className="flex justify-end gap-4">
               <p>Sandwich</p>
@@ -98,7 +146,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Dinners</p>
+            <p className="text-center genty text-3xl mb-4">Dinners</p>
 
             {dinnersData.map((data, index) => (
               <div key={index}>
@@ -117,7 +165,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Sandwiches</p>
+            <p className="text-center genty text-3xl mb-4">Sandwiches</p>
 
             <div className="flex justify-end gap-4">
               <p>Sandwich</p>
@@ -136,7 +184,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Salads</p>
+            <p className="text-center genty text-3xl mb-4">Salads</p>
 
             {saladsData.map((data, index) => (
               <div key={index}>
@@ -146,7 +194,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Fried Chicken</p>
+            <p className="text-center genty text-3xl mb-4">Fried Chicken</p>
 
             {friedChickenData.map((data, index) => (
               <div key={index}>
@@ -156,7 +204,7 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Clubs</p>
+            <p className="text-center genty text-3xl mb-4">Clubs</p>
 
             <div className="flex justify-end gap-4">
               <p>Sandwich</p>
@@ -175,7 +223,9 @@ export default function MenuSection() {
           </div>
 
           <div className="border border-light-green p-8 rounded-xl overflow-hidden">
-            <p className="text-center genty text-3xl">Breakfast Sandwiches</p>
+            <p className="text-center genty text-3xl mb-4">
+              Breakfast Sandwiches
+            </p>
 
             {breakfastSandwichesData.map((data, index) => (
               <div key={index}>
